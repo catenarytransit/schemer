f84197cbc5abc21ecc9fa47bf4cafc25532aca22
Samuel Sharp <samuelsharp4@gmail.com>
2024-04-03 17:11:49 -0700

    Initial commit

---
+++ src/lib/load.ts
@@ -0,0 +1,95 @@
+import { parse } from "yaml";
+
+export type RouteTree = {
+    name: string,
+    agency: string,
+    globalbasepath: string,
+    schemebasepath: string,
+    routes: {
+        [route_id: string]: RouteSync[]
+    }
+}
+
+export type RouteSync = {
+    name: string,
+    audio: string[]
+}
+
+let globals: any;
+
+export async function loadIndex() {
+    let raw_globals = await fetch("/data/globals.yaml");
+    globals = parse(await raw_globals.text());
+
+    let raw_index = await fetch("/data/index.yaml");
+    let index = parse(await raw_index.text());
+
+    return index;
+}
+
+export async function loadScheme(scheme: string) {
+    let raw = await fetch(scheme);
+    let raw_scheme = parse(await raw.text());
+
+    let tree: RouteTree = {
+        "name": raw_scheme["NAME"],
+        "agency": raw_scheme["AGENCY"],
+        "globalbasepath": globals["BASEPATH"],
+        "schemebasepath": raw_scheme["BASEPATH"],
+        "routes": {}
+    }
+
+    Object.values(raw_scheme["ROUTE"] as string[][]).forEach((route: string[], route_index: number) => {
+        let route_id = Object.keys(raw_scheme["ROUTE"])[route_index]
+        // @ts-ignore
+        tree.routes[route_id] = []
+        route.forEach((sync_id: string) => {
+            let sync;
+            if (raw_scheme["SYNC"][sync_id]) {
+                sync = raw_scheme["SYNC"][sync_id] as any
+            } else {
+                sync = raw_scheme["SYNC"]["DEFAULT"] as any
+            }
+            // @ts-ignore
+            Object.values(sync).forEach((stage: string[], stage_index: number) => {
+                // @ts-ignore
+                let raw_sync_audios = Object.values(stage)[0] as string[]
+                let sync_audios: string[] = []
+                raw_sync_audios.forEach((audio_id: string) => {
+                    if (audio_id.includes("*")) {
+                        audio_id = audio_id.replaceAll("*", sync_id)
+                    }
+                    if (audio_id.startsWith("$")) {
+                        if (audio_id.includes('[')) {
+                            let audio_id_split = audio_id.split('[')
+                            let audio_id_key = audio_id_split[0].replace("$", "")
+                            let audio_id_args = audio_id_split[1].replace("]", "").split(",")
+                            let audio_with_args = globals[audio_id_key].map((audio: string) => {
+                                if (!audio.includes("ARG")) return globals["BASEPATH"] + audio
+                                let audio_with_args = audio
+                                audio_id_args.forEach((arg: string) => {
+                                    audio_with_args = audio_with_args.replace("ARG[" + audio_id_args.indexOf(arg) + "]", raw_scheme["BASEPATH"] + arg)
+                                })
+                                return audio_with_args
+                            })
+                            sync_audios.push(...audio_with_args)
+                        } else {
+                            globals[audio_id.replace("$", "")].forEach((audio: string) => {
+                                sync_audios.push(globals["BASEPATH"] + audio)
+                            })
+                        }
+                    } else {
+                        sync_audios.push(raw_scheme["BASEPATH"] + audio_id)
+                    }
+                })
+                // @ts-ignore
+                tree.routes[route_id].push({
+                    name: sync_id + ":" + Object.keys(stage)[0],
+                    audio: sync_audios
+                })
+            })
+        })
+    })
+
+    return tree
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,328 @@
+<script lang="ts">
+	import { loadIndex, loadScheme, type RouteTree, type RouteSync } from '$lib/load';
+	let scheme: Promise<RouteTree>;
+	let index: Promise<any> = loadIndex();
+
+	export let global_basepath: string = '';
+	export let scheme_basepath: string = '';
+
+	export let active_route: RouteSync[] = [];
+	export let active_route_name: string = 'RESERVED:SELECT-SCHEME';
+
+	export let active_sync: number = 0;
+	export let active_sync_name: string = '---:DEFAULT';
+
+	export let set_sync_mode: boolean = false;
+
+	export let date_time: string = `${new Date().toLocaleTimeString()} ${new Date().toLocaleDateString()}`;
+
+	setInterval(() => {
+		date_time = `${new Date().toLocaleTimeString()} ${new Date().toLocaleDateString()}`;
+	}, 1000)
+
+	const chime = '/assets/chime.mp3';
+	const alt_chime = '/assets/altchime.wav';
+	let audio: HTMLAudioElement;
+
+	const play_audios = (audios: string[]) => {
+		audio = new Audio();
+		let index = -1;
+
+		const play_next = () => {
+			index++;
+			if (audios[index].includes('<PAUSE>')) {
+				setTimeout(() => {
+					play_next();
+				}, 400);
+				return;
+			}
+			if (audios[index].includes('<TTS>')) {
+				let text = audios[index].split('<TTS>')[1].trim();
+				let utterance = new SpeechSynthesisUtterance(text);
+				utterance.lang = 'en-US';
+				utterance.rate = 0.8;
+				utterance.pitch = 1.2;
+				speechSynthesis.speak(utterance);
+				utterance.onend = () => {
+					play_next();
+				};
+			} else {
+				audio.src = audios[index];
+				audio.currentTime = 0;
+				audio.play();
+
+				if (index < audios.length - 1) {
+					let next_audio = new Audio(audios[index + 1]);
+					next_audio.preload = 'auto';
+					next_audio.load();
+					next_audio = new Audio(audios[index + 2]);
+					next_audio.preload = 'auto';
+					next_audio.load();
+				}
+			}
+		};
+
+		if (audios.length) {
+			play_next();
+
+			audio.addEventListener('ended', () => {
+				if (index < audios.length) {
+					play_next();
+				}
+			});
+		}
+	};
+</script>
+
+<div id="app">
+	{#if active_route_name === 'RESERVED:SELECT-SCHEME'}
+		<div id="home">
+			{#await index}
+				<h1>Loading...</h1>
+			{:then tree}
+				<h1>{tree.NAME}</h1>
+				{#each tree.SCHEMES as schemeopt}
+					<button
+						on:click={() => {
+							scheme = loadScheme(schemeopt.URL);
+							active_route_name = 'RESERVED:SELECT-ROUTE';
+						}}
+					>
+						{schemeopt.NAME}
+					</button>
+				{/each}
+			{:catch error}
+				Error loading scheme: {error.message}
+			{/await}
+		</div>
+	{:else if active_route_name === 'RESERVED:SELECT-ROUTE'}
+		<div id="home">
+			{#await scheme}
+				<h1>Loading...</h1>
+			{:then tree}
+				<h1>{tree.name}</h1>
+				{#each Object.keys(tree.routes) as route}
+					<button
+						on:click={() => {
+							active_route_name = route;
+							active_route = tree.routes[route];
+							active_sync = 0;
+							global_basepath = tree.globalbasepath;
+							scheme_basepath = tree.schemebasepath;
+						}}
+					>
+						{route}
+					</button>
+				{/each}
+			{:catch error}
+				Error loading scheme: {error.message}
+			{/await}
+		</div>
+	{:else}
+		<div id="status">
+			<div class="statusvalue">
+				<span class="statusfield">SYNC</span>
+				<br />
+				<span class="statuscontent" style:color="var(--seashore)">{active_sync_name.split(':')[0]}</span>
+				<br />
+				<span style:color={active_sync_name.split(':')[1] == 'DEFAULT' ? 'black': 'var(--gray)'} class="statussubcontent">{active_sync_name.split(':')[1]}</span>
+			</div>
+			<div class="statusvalue">
+				<span class="statusfield">TIME</span>
+				<br />
+				<span class="statuscontent">{date_time}</span>
+			</div>
+			<div class="statusvalue">
+				<span class="statusfield">ROUTE</span>
+				<br />
+				<span class="statuscontent">{active_route_name}</span>
+			</div>
+            <br />
+			{#if set_sync_mode}
+                <select
+                    on:change={(e) => {
+                        active_sync = parseInt(e.currentTarget.value);
+                        active_sync_name = '---:DEFAULT';
+                        set_sync_mode = false;
+                    }}
+                >
+					<option selected>---</option>
+                    {#each Object.keys(active_route) as sync}
+                        <option value={sync}>{Object.values(active_route)[parseInt(sync)].name.replace(':', ' - ')}</option>
+                    {/each}
+                </select>
+			{/if}
+		</div>
+		<div id="controller">
+			<button
+				on:click={() => {
+					if (audio) {
+						audio.pause();
+					}
+					let chime_audio = new Audio(chime);
+					chime_audio.play();
+				}}
+			>
+				CHIME
+			</button>
+			<br />
+			<button
+				on:click={() => {
+					if (audio) {
+						audio.pause();
+					}
+					let chime_audio = new Audio(alt_chime);
+					chime_audio.play();
+				}}
+			>
+				ALT CHIME
+			</button>
+			<br />
+			<button
+				on:click={() => {
+					set_sync_mode = !set_sync_mode;
+				}}
+			>
+				SET
+			</button>
+		</div>
+		<button
+			id="next"
+			on:click={() => {
+				if (audio) {
+					audio.pause();
+				}
+				active_sync_name = active_route[active_sync].name;
+				let chime_index = active_route[active_sync].audio.indexOf(global_basepath + '<CHIME>');
+				if (chime_index > -1) {
+					active_route[active_sync].audio[chime_index] = chime;
+				}
+
+				let alt_chime_index = active_route[active_sync].audio.indexOf(global_basepath + '<ALTCHIME>');
+				if (alt_chime_index > -1) {
+					active_route[active_sync].audio[alt_chime_index] = alt_chime;
+				}
+
+				play_audios(active_route[active_sync].audio);
+				if (active_sync < active_route.length - 1) {
+					active_sync++;
+				}
+			}}
+		>
+			<img src="/assets/next.svg" alt="Next audio" height="90" />
+		</button>
+	{/if}
+</div>
+
+<style>
+	:global(:root) {
+		--darksky: #0a233f;
+		--seashore: #42a7c5;
+		--gray: #5e5e5e;
+	}
+
+	:global(body) {
+		background: black;
+		color: white;
+		display: flex;
+		align-items: center;
+		height: 100vh;
+		padding: 0;
+		margin: 0;
+		font-size: 1.3em;
+		overflow: hidden;
+	}
+
+	* {
+		font-family: VT323, sans-serif !important;
+	}
+
+	:global(button) {
+		background: var(--darksky);
+		border: 3px solid var(--seashore);
+		color: white;
+		font-size: 1.2em;
+		padding: 0.5rem;
+		margin: 3px;
+		border-radius: 0.5rem;
+		cursor: pointer;
+		font-weight: bold;
+	}
+
+	@media (max-width: 900px) {
+		:global(body) {
+			font-size: 0.8em;
+			align-items: flex-start;
+			margin-top: 40px;
+		}
+
+		#controller button {
+			width: 70px !important;
+			height: 70px !important;
+			margin-top: 0.7rem;
+		}
+	}
+
+	#home {
+		margin-left: 30px;
+	}
+
+	#status {
+		min-width: 60vw;
+		padding: 40px;
+		text-align: left;
+	}
+
+	.statusfield {
+		font-size: 2em;
+	}
+
+	.statuscontent {
+		font-size: 2.4em;
+	}
+
+	.statussubcontent {
+		font-size: 1.8em;
+		color: var(--gray);
+	}
+
+	.statusvalue {
+		margin: 25px;
+	}
+
+	#controller {
+		position: fixed;
+		bottom: 20px;
+		right: 20px;
+	}
+
+	#controller button {
+		width: 120px;
+		height: 120px;
+		margin: 0;
+		margin-left: 10px;
+		font-size: 1.6em;
+		text-align: center;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	#next {
+		position: fixed;
+		bottom: 20px;
+		left: 20px;
+		width: 160px;
+		height: 160px;
+		margin: 0;
+	}
+
+    select {
+        margin-top: 20px;
+        background-color: var(--blue);
+        padding: 10px;
+        border-radius: 10px;
+        border: 3px solid white;
+        color: white;
+    }
+</style>
